9d407ac5c5e626f0b8e30c15678604a075b0d875
Nick <143449854+MicrowaveBallin@users.noreply.github.com>
2025-03-31 17:36:21 -0400

    cotroller worked on

--- Store Management System/StoreManagementApp/index.php
+++ Store Management System/StoreManagementApp/index.php
@@ -1,3 +1,17 @@
 <?php
 
 include_once 'Util/cdebug';
+
+
+$controller = (isset($_GET['controller'])) ? $_GET['controller'] : "home";
+
+if ($controller == "home") {
+    include "Views/mainpage.php";
+    exit;
+}
+
+$controllerClassName = ucfirst($controller) . "Controller";
+include_once "Controllers/$controllerClassName.php";
+
+$ct = new $controllerClassName();
+$ct->route();
