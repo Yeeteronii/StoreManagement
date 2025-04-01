912b969a9836d39de05809f73bed2b5a8c9fee16
ParthPatelA <parthschool20s@gmail.com>
2025-04-01 12:42:07 -0400

    Category list and view

--- Store Management System/StoreManagementApp/Models/Product.php
+++ Store Management System/StoreManagementApp/Models/Product.php
@@ -59,7 +59,7 @@ class Product {
     public static function view($productId = -1)
     {
         $list = [];
-        $sql = "SELECT * FROM `products` WHERE `productId` " . $productId . " LIMIT 1";
+        $sql = "SELECT * FROM `products` WHERE `productId`  = " . $productId . " LIMIT 1";
 
         $conn = Model::connect();
         $result = $conn->query($sql);
